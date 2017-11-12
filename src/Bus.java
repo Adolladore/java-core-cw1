8da3d7624c3b32fe7ddb81202f8889b690342ebf
@volodymyr.manko <123enot@gmail.com>
2017-11-12 13:26:08 +0200

    Created model

---
+++ src/Bus.java
@@ -0,0 +1,25 @@
+public class Bus {
+    private String name;
+    private Stop currentStop;
+
+    public Bus(String name, Stop currentStop) {
+        this.name = name;
+        this.currentStop = currentStop;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Stop getCurrentStop() {
+        return currentStop;
+    }
+
+    public void setCurrentStop(Stop currentStop) {
+        this.currentStop = currentStop;
+    }
+}
---
+++ src/Path.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Path {
+    private Integer pathNumber;
+    private List<Stop> stops = new ArrayList<>();
+    private List<Bus> buses = new ArrayList<>();
+
+    public List<Bus> getBuses() {
+        return buses;
+    }
+
+    public void setBuses(List<Bus> buses) {
+        this.buses = buses;
+    }
+
+    public Path(Integer pathNumber) {
+        this.pathNumber = pathNumber;
+    }
+
+    public Integer getPathNumber() {
+        return pathNumber;
+    }
+
+    public void setPathNumber(Integer pathNumber) {
+        this.pathNumber = pathNumber;
+    }
+
+    public List<Stop> getStops() {
+        return stops;
+    }
+
+    public void setStops(List<Stop> stops) {
+        this.stops = stops;
+    }
+
+    public void addStop(Stop stop) {
+        stops.add(stop);
+    }
+
+    public void addBus(Bus bus) {
+        buses.add(bus);
+    }
+}
